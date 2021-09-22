189fcc5a66f321659169ae436e03efeed6f1923c
Markus Rathgeb <maggu2810@gmail.com>
2021-09-22 17:20:47 +0200

    fix find catch
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- cmake/Findcatch.cmake
+++ cmake/Findcatch.cmake
@@ -1,4 +1,7 @@
-find_path( catch_INCLUDE catch.hpp HINTS "${PROJECT_SOURCE_DIR}/dependency/catch/single_include/catch2" "/usr/include/catch2/single_include" "/usr/include/catch2" "/usr/local/include/catch2/single_include" "/usr/local/include/catch2" "/opt/local/include/catch2/single_include" "/opt/local/include/catch2" )
+find_path( catch_INCLUDE catch2/catch.hpp )
+if ( catch_INCLUDE )
+    set( catch_INCLUDE ${catch_INCLUDE}/catch2 )
+endif ( )
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(catch DEFAULT_MSG catch_INCLUDE)
